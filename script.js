4b0168b03f113f2a1b900a2e7ea830469130554b
Preston C <pchandler858@gmail.com>
2023-02-17 14:54:09 -0800

    added nav bar and score to html and css

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
+const scoreContainerElement = document.getElementById("score-container");
 const answerButtonsElement = document.getElementById("answer-buttons");
 let shuffledQuestions, currentQuestionIndex;
 
@@ -61,6 +62,10 @@ function selectAnswer(e) {
     startButton.innerText = "Try Again?";
     startButton.classList.remove("hide");
     nextButton.classList.add("hide");
+    scoreContainerElement.classList.remove("hide");
+    questionContainerElement.classList.add("hide");
+
+    clearStatusClass(document.body);
   }
 }
 
@@ -80,57 +85,75 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: "Question #1",
+    question: "What does HTML stand for?",
     answers: [
-      { text: "a", correct: true },
-      { text: "b", correct: false },
-      { text: "c", correct: false },
-      { text: "d", correct: false },
+      { text: "Hyper Text Markup Language", correct: true },
+      { text: "High Tech Markup Language", correct: false },
+      { text: "Hyper Tabular Markup Language", correct: false },
+      { text: "None of the above", correct: false },
     ],
   },
   {
-    question: "Question #2",
+    question: "What does CSS stand for?",
     answers: [
-      { text: "a", correct: true },
-      { text: "b", correct: false },
-      { text: "c", correct: false },
-      { text: "d", correct: false },
+      { text: "Creative Style Sheets", correct: false },
+      { text: "Cascading Style Sheets", correct: true },
+      { text: "Colorful Style Sheets", correct: false },
+      { text: "Computer Style Sheets", correct: false },
     ],
   },
   {
-    question: "Question #3",
+    question: "What is the purpose of a CSS reset?",
     answers: [
-      { text: "a", correct: true },
-      { text: "b", correct: false },
-      { text: "c", correct: false },
-      { text: "d", correct: false },
+      { text: "To eliminate browser inconsistencies", correct: true },
+      { text: "To create a baseline for the page design", correct: false },
+      { text: "To clear all styling rules from the page", correct: false },
+      { text: "To reduce page loading times", correct: false },
     ],
   },
   {
-    question: "Question #4",
+    question: "Which tag is used to define a hyperlink?",
     answers: [
+      { text: "img", correct: false },
       { text: "a", correct: true },
-      { text: "b", correct: false },
-      { text: "c", correct: false },
-      { text: "d", correct: false },
+      { text: "div", correct: false },
+      { text: "span", correct: false },
     ],
   },
-  {
-    question: "Question #5",
-    answers: [
-      { text: "a", correct: true },
-      { text: "b", correct: false },
-      { text: "c", correct: false },
-      { text: "d", correct: false },
-    ],
-  },
-  {
-    question: "Question #6",
-    answers: [
-      { text: "a", correct: true },
-      { text: "b", correct: false },
-      { text: "c", correct: false },
-      { text: "d", correct: false },
-    ],
-  },
+  // {
+  //   question: "What is a responsive website?",
+  //   answers: [
+  //     { text: "A website that loads quickly", correct: false },
+  //     { text: "A website that is optimized for search engines", correct: false },
+  //     { text: "A website that adapts to different screen sizes", correct: true },
+  //     { text: "A website that is accessible to people with disabilities", correct: false },
+  //   ],
+  // },
+  // {
+  //   question: "What is the purpose of the alt attribute on an image tag?",
+  //   answers: [
+  //     { text: "To provide a title for the image", correct: false },
+  //     { text: "To specify the width of the image", correct: false },
+  //     { text: "To define a caption for the image", correct: false },
+  //     { text: "To describe the image for screen readers", correct: true },
+  //   ],
+  // },
+  // {
+  //   question: "What is a CSS framework?",
+  //   answers: [
+  //     { text: "A collection of CSS styles that can be reused across a website", correct: true },
+  //     { text: "A tool for optimizing website performance", correct: false },
+  //     { text: "A set of rules for organizing HTML code", correct: false },
+  //     { text: "A system for tracking user interactions on a website", correct: false },
+  //   ],
+  // },
+  // {
+  //   question: "What is the box model in CSS?",
+  //   answers: [
+  //     { text: "A way of organizing CSS styles using boxes", correct: false },
+  //     { text: "A model for designing user interfaces", correct: false },
+  //     { text: "A model for describing the layout of HTML elements", correct: true },
+  //     { text: "A way of measuring page loading times", correct: false },
+  //   ],
+  // },
 ];
