dc9542efbf61fb507dff6a2143d572ee83e322c0
Preston C <pchandler858@gmail.com>
2023-02-21 15:17:42 -0800

    chandged the a btn to a button element and added event listener in highScore.

--- highScores.js
+++ highScores.js
@@ -1,5 +1,10 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const homeBtn = document.getElementById("home-btn");
+
+homeBtn.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
 
 highScoresList.innerHTML = highScores
   .map((score) => {
