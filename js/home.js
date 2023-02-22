2355a61e547e6503c039b88b06f55296a39dc724
Preston C <pchandler858@gmail.com>
2023-02-21 19:35:59 -0800

    gh pages bug troubleshoot

--- js/home.js
+++ js/home.js
@@ -2,8 +2,8 @@ const takeQuizBtn = document.getElementById("takeQuiz-btn");
 const highScoresBtn = document.getElementById("highScores-btn");
 
 takeQuizBtn.addEventListener("click", function () {
-  window.location.href = "../html/quiz.html";
+  window.location.href = "/html/quiz.html";
 });
 highScoresBtn.addEventListener("click", function () {
-  window.location.href = "../html/highScores.html";
+  window.location.href = "/html/highScores.html";
 });
